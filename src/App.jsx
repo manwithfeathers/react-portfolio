a2db5ff0325ca185c5b724018dc22e5f96f067ac
Adam Parkinson <a.d.parkinson@gmail.com>
2026-01-19 15:19:44 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [album, showAlbums] = useState(true)
+
+  return (
+    <>
+      <button onClick = {() => showAlbums(!album)}>{album ? "Hide" : "View"} Discography</button>
+      <div>
+        {album && (
+          <div>
+            <iframe 
+              style= {{ border: "0", width: "100%", height: "120px"}} 
+              src="https://bandcamp.com/EmbeddedPlayer/album=274195578/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" 
+              seamless>
+              <a 
+                href="https://thedanelaw.bandcamp.com/album/the-black-path">The Black Path by Dane Law
+              </a>
+            </iframe>
+
+            <iframe 
+              style= {{ border: "0", width: "100%", height: "120px"}} 
+              src="https://bandcamp.com/EmbeddedPlayer/album=4027572572/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" 
+              seamless>
+             <a href="https://thedanelaw.bandcamp.com/album/blue-forty-six">Blue Forty-Six by Dane Law
+             </a>
+            </iframe>
+
+            <iframe 
+              style={{ border: "0", width: "100%", height: "120px"}} 
+              src="https://bandcamp.com/EmbeddedPlayer/album=269748602/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" 
+              seamless>
+              <a href="https://thedanelaw.bandcamp.com/album/algorithmic-music-for-synthesised-strings">Algorithmic Music for Synthesised Strings by Dane Law
+                </a>
+            </iframe>
+
+          </div>
+          )}
+      </div>
+      
+    </>
+  )
+}
+
+export default App
